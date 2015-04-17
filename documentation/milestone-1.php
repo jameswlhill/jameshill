b7e71c110dfe1101e4561cb4ba8c447b71793a01
James Hill <james.w.l.hill@gmail.com>
2015-04-17 07:51:04 -0600

    Populated milestone-1.php with the purpose, intended audience, and goal of my PWP

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -6,3 +6,38 @@
  * Time: 9:39 PM
  */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>PWP Schema</title>
+	</head>
+	<body>
+		<header>
+			<h1><em>PWP Schema</em></h1>
+		</header>
+		<section>
+			<h3>Purpose:</h3>
+			<p>
+				The purpose of my personal web page project will be to document and showcase
+				my professional development through the DDC boot camp, as well as my web dev portfolio.
+			</p>
+		</section>
+		<section>
+			<h3>Audience:</h3>
+			<p>
+				The intended audience for my personal web page will be potential employers, clients,
+				as well as colleagues, family, and friends.
+			</p>
+		</section>
+		<section>
+			<h3>Goal:</h3>
+			<p>
+				The goal of my project is to allow me to get my foot in the door of the tech industry.
+				I want to have something tangible to present to my future employers and clients which
+				will demonstrate my technical abilities in the area of front-end and back-end development.
+			</p>
+		</section>
+	</body>
+</html>
