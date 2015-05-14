ab7ef938db016c97ac49dfb6fd8eadcdd178089e
James Hill <james.w.l.hill@gmail.com>
2015-05-13 20:51:26 -0600

    Created milestone-3 documentation containing my pwp wireframes for both mobile and desktop

---
+++ documentation/milestone-3.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Milestone-3 wireframes for PWP
+ *
+ * @author James Hill <james@appists.com>
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Milestone-3</title>
+	</head>
+	<body>
+		<h1>James Hill's PWP Wireframe</h1>
+		<section>
+			<h3>Mobile Wireframe</h3>
+			<figure>
+				<img src="../img/mobilehome.png">
+				<figcaption>
+					'Home' div with a header, sticky-footer, and top-right nav drop down menu containing
+					social profile links as well as a link to download a .pdf of my resume. The centerpiece
+					will be a zia symbol terminating in links to my internal site navs.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/mobileabout.jpg">
+				<figcaption>
+					'About Me' div with a header, footer, and top-right nav drop down menu containing
+					internal site navs. This will feature a professional head shot floated amongst introductory
+					text, and perhaps a speech bubble greeting. Below, a bootstrap col-4 spread consisting
+					of descriptive thumbnails and captions outlining some of my personal interests, such
+					as various web technologies, horticulture, and zymurgy.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/mobileeducation.png">
+				<figcaption>
+					'Education' div featuring an animated JQuery time line of my educational milestones. Each node
+					will present a modal window with a brief description of my achievements at each school.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/mobileworks.png">
+				<figcaption>
+					'Works' div will contain an image gallery, each thumbnail of which will be a link to a
+					third-party site. These might be presented by an animated carousel of
+					screenshots from various projects I've been a part of.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/mobilecontact.png">
+				<figcaption>
+					'Contact' div will contain bootstrap icons for social profile links, as well as
+					a simple form requesting sender's name, preferred e-mail, and a text box to be
+					emailed to my Appists account.
+				</figcaption>
+			</figure>
+		</section>
+		<section>
+			<h3>Desktop Wireframe</h3>
+			<figure>
+				<img src="../img/desktophome.png">
+				<figcaption>
+					'Home' desktop version div with a header, sticky-footer, and social profile links. The centerpiece
+					will be a zia symbol terminating in links to my internal site navs. On the side, if consistent
+					with responsive design, I'd like to have a scaled-down image of my resume, which can be viewed
+					as a modal window when clicked. The option to download the .pdf should still be part of the
+					top drop down menu.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/desktopabout.png">
+				<figcaption>
+					'About Me' div with a header, footer, and top-right nav drop down menu containing
+					internal site navs. This will feature a professional head shot floated amongst introductory
+					text, and perhaps a speech bubble greeting. Below, a bootstrap col-4 spread consisting
+					of descriptive thumbnails and captions outlining some of my personal interests, such
+					as various web technologies, horticulture, and zymurgy.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/desktopeducation.png">
+				<figcaption>
+					'Education' div featuring a cursor-animated JQuery time line of my educational milestones.
+					Each node will present a modal window with a brief description of my achievements at each school.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/desktopworks.png">
+				<figcaption>
+					'Works' div will contain an image gallery, each thumbnail of which will be a link to a
+					third-party site. These might be presented by an animated carousel of
+					screenshots from various projects I've been a part of.
+				</figcaption>
+			</figure>
+			<figure>
+				<img src="../img/desktopcontact.png">
+				<figcaption>
+					'Contact' div will contain bootstrap icons for social profile links, as well as
+					a simple form requesting sender's name, preferred e-mail, and a text box to be
+					emailed to my Appists account.
+				</figcaption>
+			</figure>
+		</section>
+	</body>
+</html>
